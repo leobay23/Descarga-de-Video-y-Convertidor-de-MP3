a626347f1412b91af12c7f3e74cfbb7f6116f325
Ghost (Leobay) <153404267+leobay23@users.noreply.github.com>
2024-05-13 22:17:14 -0300

    Add files via upload

--- youtube.py
+++ youtube.py
@@ -1,43 +1,141 @@
+from ast import Str
 from pytube import YouTube
-from tkinter import *
-from tkinter import messagebox as MessageBox
+import os
+from moviepy.editor import *
+import conf_management as cfg
 
-def accion():
-    enlace = videos.get()
+
+parent_dir = ''
+
+# Windows
+# def clear(): os.system('cls')
+
+# Linux
+def clear(): os.system('clear')
+
+
+def menu_inicio() -> Str:
+    clear()
+    return input('Selecciona una opción:\n1. Descargar enlace YouTube\n2. Descargar fichero\n3. Salir\n\nOpción (1|2|3): ')
+
+
+def seleccionar_accion() -> Str:
+    return input('¿Qué deseas hacer?\n\n1. Descarga rápida vídeo y audio\n2. Descarga vídeo seleccionando calidad\n3. Descarga audio\n\nOpción (1|2|3): ')    
+
+
+def print_proceso_terminado():
+    print('Descarga completada')
+
+    print('-------------------------------------------------------')
+
+
+def pide_url() -> Str:
+    return input('Url vídeo: ')
+
+
+def procesa_fichero(ruta) -> list:
+    list_enlaces = []
+
+    file_object = open(ruta, 'r')
+
+    [list_enlaces.append(linea) for linea in file_object]
+
+    file_object.close()
+
+    return list_enlaces
+
+
+def inicia_proceso_descarga(link, job, loop):
+    contador = 1
+
     try:
-        video = YouTube(enlace)
-        descarga = video.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
-        descarga.download()
-        MessageBox.showinfo("Descarga completada", "El video ha sido descargado con éxito, para convertirlo en mp3, esta este link, escribilo al pie de la letra https://www.freeconvert.com/es/mp4-to-mp3")
-    except Exception as e:
-        MessageBox.showerror("Error", str(e))
+        yt = YouTube(link)
+    except:
+        print('Error en el link: {0}'.format(link))
+        return
 
-def popup():
-    MessageBox.showinfo("Sobre mi", "Ghost, enlace de Instagram:\nhttps://www.instagram.com/ghost_007_23")
+    nombre_video = ''
 
-root = Tk()
-root.config(bd=15)
-root.title("Script descargar videos")
+    print('')
+    print('-------------------------------------------------------')
+    print('Título: {0}'.format(yt.title))
+    print('Autor: {0}'.format(yt.author))
+    print('')
 
-imagen = PhotoImage(file="yt.png")
-foto = Label(root, image=imagen, bd=0)
-foto.grid(row=0, column=0)
+    video_y_audio = yt.streams.filter(progressive=True).order_by('resolution').desc().first()
+    vids = yt.streams.filter(mime_type="video/mp4").order_by('resolution').desc()
 
-menubar = Menu(root)
-root.config(menu=menubar)
-helpmenu = Menu(menubar, tearoff=0)
+    if job == '1':  # Vídeo y audio rápido
+        video_y_audio.download(parent_dir + '/video')
+        print_proceso_terminado()
+    elif job == '3': # Solo audio
+        ruta_fin = yt.streams.get_audio_only().download(parent_dir + '/audio')
+        audioclip = AudioFileClip(ruta_fin)
+        audioclip.write_audiofile(audioclip.filename.replace('.mp4', '.mp3'))
 
-menubar.add_cascade(label="Más Información", menu=helpmenu)
-helpmenu.add_command(label="Info del Autor", command=popup)
-menubar.add_command(label="Salir", command=root.destroy)
+        os.remove(audioclip.filename)
 
-instrucciones = Label(root, text="Pega la url del video de YouTube y descargalo\n")
-instrucciones.grid(row=0, column=1)
+        print_proceso_terminado()
+    elif job == '2':  # Descargar vídeo y audio seleccionando calidad    
+        num_video_descargar = 1
 
-videos = Entry(root)
-videos.grid(row=1, column=1)
+        if not loop:
+            print('')
+            print('Seleccionar vídeo (1..{0})'.format(len(vids)))
 
-boton = Button(root, text="Descargar", command=accion)
-boton.grid(row=2, column=1)
+            for video in vids:
+                print('    ({1}) - {0}'.format(video, contador))
+                contador += 1
 
-root.mainloop()
+            print('')
+
+            num_video_descargar = int(input('Nº vídeo: '))
+
+        num_video_descargar -= 1
+
+        nombre_video = vids[num_video_descargar].default_filename
+        nombre_video_final = 'f_' + nombre_video
+        vids[num_video_descargar].download(parent_dir + '/video')
+
+        yt.streams.get_audio_only().download(parent_dir + '/audio')
+
+        audioclip = AudioFileClip(parent_dir + '/audio/' + nombre_video)
+
+        videoclip2 = VideoFileClip(parent_dir + '/video/' + nombre_video)
+        videoclip2 = videoclip2.set_audio(audioclip)
+
+        videoclip2.write_videofile(parent_dir + '/video/' + nombre_video_final)
+
+        os.remove(videoclip2.filename)
+        os.remove(audioclip.filename)
+
+
+if __name__ == "__main__":
+    tarea = 0
+    job = 0
+    enlaces = []
+    parent_dir = cfg.get_ruta_descargas()  # Obtener ruta donde se guardan las descargas
+
+    while tarea != '3':
+        enlaces.clear()
+        tarea = menu_inicio()  # Mostrar menú inicial
+        loop = False
+
+        clear()
+
+        # Llenar una lista con los enlaces a descargar
+        if tarea == '1':  # Un solo vídeo
+            enlaces.append(pide_url())
+        elif tarea == '2': # Fichero de texto
+            loop = True
+            ruta_fichero = input('Ruta fichero enlaces: ')
+            enlaces = procesa_fichero(ruta_fichero)
+
+        # Seleccionar qué tipo de descarga hacer
+        while job not in ['1', '2', '3']:
+            clear()
+            job = seleccionar_accion()
+
+        [inicia_proceso_descarga(x, job, loop) for x in enlaces]
+
+    print_proceso_terminado()
